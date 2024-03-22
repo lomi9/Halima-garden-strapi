d355dc9e5c3de1c074d337026823bba766488cef
lomi9 <119067005+lomi9@users.noreply.github.com>
2024-03-22 02:12:10 +0100

    fix : clerk

--- src/api/clerk-webhooks/controllers/clerk-webhooks.js
+++ src/api/clerk-webhooks/controllers/clerk-webhooks.js
@@ -1,16 +1,20 @@
+// clerk-webhook.js
 module.exports = {
   async handleClerkWebhook(ctx) {
-    const { emailAddress } = ctx.request.body.data;
+    const { emailAddress } = ctx.request.body.data; // Assurez-vous que le chemin est correct
 
     try {
       // Créer un nouveau 'Clerk-user' avec l'email reçu de Clerk
-      const newUser = await strapi.services["clerk_users"].create({
+      // Assurez-vous que le service est nommé correctement. Il doit correspondre à votre modèle Strapi
+      const newUser = await strapi.services["clerk-user"].create({
         email: emailAddress,
       });
       return ctx.created(newUser);
     } catch (error) {
+      console.error(error); // Ajout pour le débogage
       return ctx.badRequest(
-        "Une erreur est survenue lors de la création de l'utilisateur Clerk."
+        "Une erreur est survenue lors de la création de l'utilisateur Clerk.",
+        { error }
       );
     }
   },
